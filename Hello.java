ad1b1fed73a1ac1a938da5b83bed37d7ff46e28b
Benjamin Arva <arvabeni08@gmail.com>
2017-07-16 12:21:30 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,7 @@
 public class Hello {
 
 public static void main(String[] args) {
-System.out.println("Hello World! fic----874/
-                  
+System.out.println("Hello World! fic----874/");
                   ///84--l");
 }
 }
